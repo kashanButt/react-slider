7c11e123a8b5dfe766b28846e959ee27a549c0a0
unknown <kashanbutt566gmail.com>
2023-07-18 17:13:26 +0500

    finished slider

---
+++ src/Components/Container.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from './Slider/Slider'
+
+const Container = () => {
+  return (
+    <div className='container'>
+        <Slider />
+    </div>
+  )
+}
+
+export default Container
